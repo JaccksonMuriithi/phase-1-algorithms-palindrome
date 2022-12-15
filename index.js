cee983cafb987da48dff54d8263f720ec0b493a1
JacksonMuriithi <jacksonmuriithi299@mail.com>
2022-12-15 12:59:36 +0300

    completed lab

--- index.js
+++ index.js
@@ -23,3 +23,39 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+function isPalindrome(word) {
+  // Write your algorithm here
+}
+
+/* 
+  Add your pseudocode here
+*/
+
+/*
+  Add written explanation of your solution here
+*/
+
+// You can run `node index.js` to view these console logs
+if (require.main === module) {
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
+
+module.exports = isPalindrome;
+
+
+let word = 'mom';
+function isPalindrome(word) {
+  // Write your algorithm here
+  if(word.split("").reverse().join("") === word){
+    return true; 
+  } else{
+    return false;
+  }
+}
